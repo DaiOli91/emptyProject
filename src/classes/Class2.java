752bce5dbf27fd8589593a7948c1fcd7b71fe5e9
DaiOli <oliveratup9119@gmail.com>
2021-03-24 13:10:16 -0300

    first commit

---
+++ src/classes/Class2.java
@@ -0,0 +1,10 @@
+package classes;
+
+import interfaces.Interface1;
+
+public class Class2 extends Class1 implements Interface1 {
+    public int method1(){
+        return 1;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import classes.Class2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Class2 example = new Class2();
+
+        System.out.println(example.method1());
+    }
+}
